02ad9e14c6d9eb5b6c4da6ca6889be002b7f9051
Cameron Holt <captain.cholt@gmail.com>
2017-04-06 18:36:50 -0500

    Add Area.

---
+++ Area.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Area here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Area extends World
+{
+
+    /**
+     * Constructor for objects of class Area.
+     * 
+     */
+    public Area(String backgroundImage)
+    {    
+        super(500, 500, 1);
+        setBackground(backgroundImage);
+    }
+}
